8979bc71aff1c7b8dd42f7e3063b0870757ecacf
Andrew Shao <132319777+andrewshaodev@users.noreply.github.com>
2024-03-22 15:29:53 +0000

    test

--- .devcontainer/updateContentCommand.sh
+++ .devcontainer/updateContentCommand.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 echo "SECRET_KEY=$SECRET_KEY" >> .env
-echo "POSTGRES_USER=$DB_USER" >> .env
-echo "POSTGRES_DB=$DB_DB" >> .env
-echo "POSTGRES_PASSWORD=$DB_PASSWORD" >> .env
+echo "POSTGRES_USER=$POSTGRES_USER" >> .env
+echo "POSTGRES_DB=$POSTGRES_DB" >> .env
+echo "POSTGRES_PASSWORD=$POSTGRES_PASSWORD" >> .env
 echo "POSTGRES_HOST=localhost" >> .env
+echo "DEBUG=True" >> .env
+echo "ALLOWED_HOSTS=*" >> .env
 
 # echo "alias fly='flyctl'" >> ~/.bashrc
 # echo "alias fly='flyctl'" >> ~/.zshrc
