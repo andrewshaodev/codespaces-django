792184c886eb8ec171fef1e9742efae97c436e3a
Andrew Shao <132319777+andrewshaodev@users.noreply.github.com>
2024-03-22 15:17:26 +0000

    testing .env creation

--- .devcontainer/updateContentCommand.sh
+++ .devcontainer/updateContentCommand.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # echo "SECRET_KEY=$SECRET_KEY" >> .env
-# echo "POSTGRES_USER=$DB_USER" >> .env
+echo "POSTGRES_USER=$DB_USER" >> .env
 # echo "POSTGRES_DB=$DB_DB" >> .env
 # echo "POSTGRES_PASSWORD=$DB_PASSWORD" >> .env
 # echo "POSTGRES_HOST=localhost" >> .env
